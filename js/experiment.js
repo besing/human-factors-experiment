d6d2a1f5907f72b3726e6497a266d78eecd4bf61
Benni Desktop <benni.singer@gmail.com>
2016-05-24 23:19:42 +0200

    Random Color/Sound selection in progress

--- js/experiment.js
+++ js/experiment.js
@@ -16,14 +16,13 @@ var playSinoid = function playSinoid(freq, duration) { // (Hz, sec)
 };
 
 
-
 // Count Keypresses via keypress.js
 var listener = new window.keypress.Listener();
 
 var countTotalUp = [];
 var countTotalDown = [];
 
-var countKeyPress = function(keyString) {
+var countKeyPress = function (keyString) {
   listener.counting_combo(keyString, function () {
 
     if (keyString === 'up') { // TODO
@@ -43,34 +42,74 @@ countKeyPress('up');
 countKeyPress('down');
 
 
-var soundBlock = {
-  type: 'single-stim',
-  timing_response: 1500,
-  response_ends_trial: false,
-  randomize_order: true,
-  is_html: true,
+// Color HTML snippets for "prompt"
+var colorsHtml = [
+  ['0a', '<div class="color-box" id="color-0-a">color0a</div>'],
+  ['0b', '<div class="color-box" id="color-0-b">color0b</div>'],
+  ['1a', '<div class="color-box" id="color-1-a">color1a</div>'],
+  ['1b', '<div class="color-box" id="color-1-b">color1b</div>'],
+  ['1c', '<div class="color-box" id="color-1-c">color1c</div>'],
+  ['2a', '<div class="color-box" id="color-2-a">color2a</div>'],
+  ['2b', '<div class="color-box" id="color-2-b">color2b</div>'],
+  ['2c', '<div class="color-box" id="color-2-c">color2c</div>'],
+  ['3a', '<div class="color-box" id="color-3-a">color3a</div>'],
+  ['3b', '<div class="color-box" id="color-3-b">color3b</div>'],
+  ['3c', '<div class="color-box" id="color-3-c">color3c</div>']
+];
 
-  timeline: [
-    {
-      stimulus: function () {
-        playSinoid(80, 1);
-      },
-      prompt: '<div class="color-box" id="color-1-a">color1a</div>'
-    },
-    {
-      stimulus: function () {
-        playSinoid(120, 1);
-      },
-      prompt: '<div class="color-box" id="color-1-b">color1b</div>'
-    }
-  ]
-};
+
+var sinoids = [
+  [80, 'playSinoid(80, 1)'],
+  [120, 'playSinoid(120, 1)'],
+  [150, 'playSinoid(150, 1)']
+];
+
+
+var shuffleAudio = jsPsych.randomization.shuffle(sinoids);
+console.log(shuffleAudio);
+var shuffleColors = jsPsych.randomization.shuffle(colorsHtml);
+console.log(shuffleColors);
+
+
+
+// TODO : for-Schleife noch mal überarbeiten .. evtl. statt i < 33 (geht das mit untersch. großen Arrays? Vielleicht gibts ne bessere Random-Funktion, die genaue Anzahl von Array-Items erstellt (mit Duplikaten dann eben)
 
 
 
 var timeline = [];
 
-timeline.push(soundBlock);
+for (var i = 0; i < 3; i++) { // TODO : i < 33
+  var combinedStimuli = {
+    type: 'single-stim',
+    timing_response: 1500,
+    response_ends_trial: false,
+    // randomize_order: true,
+    is_html: true,
+    stimulus: shuffleAudio[i][1],
+    // prompt: [colorsHtml]
+  }
+
+  timeline.push(combinedStimuli);
+
+  /*timeline: [
+   {
+   stimulus: function () {
+   playSinoid(80, 1);
+   },
+   prompt: '<div class="color-box" id="color-1-a">color1a</div>'
+   },
+   {
+   stimulus: function () {
+   playSinoid(120, 1);
+   },
+   prompt: '<div class="color-box" id="color-1-b">color1b</div>'
+   }
+   ]*/
+};
+
+
+
+
 
 jsPsych.init({
   timeline: timeline,
